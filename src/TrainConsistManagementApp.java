59c639811d9287ca136c09d0b0e4b1466dcedf11
gnaneswar944 <gk7408@srmist.edu.in>
2026-03-25 09:27:58 +0530

    UC3:Track Unique Bogie IDs

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,23 +1,16 @@
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
 public class TrainConsistManagementApp {
  public static void main(String[] args){
      System.out.println("======================================");
-     System.out.println("   ===TrainConsistManagementApp===  ");
+     System.out.println("   ===Track Unique Bogie IDs ===  ");
      System.out.println("======================================");
-     List<String> passengerBogies = new ArrayList<>();
-     passengerBogies.add("Sleeper");
-     passengerBogies.add("AC Chair");
-     passengerBogies.add("First class");
-     System.out.print("After adding Bogies");
-     System.out.print(passengerBogies);
-     passengerBogies.remove("AC Chair");
-     System.out.print("\nAfter Removing 'AC Chair' ");
-     System.out.print(passengerBogies);
-     boolean isSleeperPresent = passengerBogies.contains("Sleeper");
-     System.out.println("\nIs 'Sleeper' bogie present? " + isSleeperPresent);
-     System.out.println("\nFinal Passenger Bogie List:");
-     System.out.println(passengerBogies);
-     System.out.println("\nSystem ready for next operations...");
+     Set<String> bogieIDs = new HashSet<>();
+     bogieIDs.add("BG104");
+     bogieIDs.add("BG103");
+     bogieIDs.add("BG102");
+     bogieIDs.add("BG101");
+     System.out.print("Bogie IDs AFter Insertion:\n");
+     System.out.print(bogieIDs);
 }
 }

eaf3aae788d6ca156df4647176dc94647c2c7d7d
gnaneswar944 <gk7408@srmist.edu.in>
2026-03-25 09:58:13 +0530

    UC5:Preserve insertion Order of Bogies

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -3,22 +3,15 @@ import java.util.List;
 public class TrainConsistManagementApp {
  public static void main(String[] args){
      System.out.println("======================================");
-     System.out.println("   ===Maintain Ordered Bogie Consist ===  ");
+     System.out.println("   ===UC 5-Preserve Insertion order of bogies ===  ");
      System.out.println("======================================");
-     List<String> trainConsist = new LinkedList<>();
-     trainConsist.add("Engine");
-     trainConsist.add("Sleeper");
-     trainConsist.add("AC");
-     trainConsist.add("Cargo");
-     trainConsist.add("Guard");
-     System.out.print("Intial Train Consist\n");
-     System.out.print(trainConsist);
-     trainConsist.set(2, "Pantry Car");
-     System.out.print("\nAfter Inserting 'pantry Car' at position 2\n");
-     System.out.print(trainConsist);
-     trainConsist.remove("Sleeper");
-     trainConsist.remove("Guard");
-     System.out.print("\nAfter removing first and last\n");
-     System.out.print(trainConsist);
+     LinkedHashSet<String> formation = new LinkedHashSet<>();
+     formation.add("Engine");
+     formation.add("Sleeper");
+     formation.add("Cargo");
+     formation.add("Guard");
+     System.out.print("Final Train Foramtion\n");
+     System.out.print(formation);
+
 }
 }
